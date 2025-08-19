2f88dece4b2dc21640067186b8a9aebc82d21a20
ShadabShahzada <126250733+ShahzadaShadabAli@users.noreply.github.com>
2025-08-19 13:25:00 +0500

    Update route.js

--- app/api/confirmation/get/route.js
+++ app/api/confirmation/get/route.js
@@ -5,6 +5,7 @@ export const GET = async () => {
     try {
         await connectDB()
         const confirmations = await Confirmation.find({})
+        console.log(confirmations)
         return new Response(JSON.stringify(confirmations), {status: 200})
     } catch (error) {
         return new Response(JSON.stringify("Failed to fetch data"), {status: 500})
